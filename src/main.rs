8181ae499901aebf32ec16cb4997163237115cda
Kiran Singh <ssnarik@gmail.com>
2023-01-14 11:08:16 -0800

    cleaned up some warnings

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ struct GatherArgs {
     path: Option<String>,
 
     #[arg(short, long, default_value = "src/")]
-    sourceCodeDirectory: Option<String>,
+    source_code_directory: Option<String>,
 }
 
 #[derive(Args)]
@@ -168,7 +168,7 @@ async fn main() {
             Beta::Gather(args) => {
                 // Gather should gather all the strings from the codebase and create a json file.
 
-                let source_directory = args.sourceCodeDirectory.clone().unwrap();
+                let source_directory = args.source_code_directory.clone().unwrap();
                 let strings_file_path = args.path.clone().unwrap();
 
                 // 1) traverse the structure and find all the js or jsx files
@@ -196,7 +196,7 @@ async fn main() {
 
                 println!("Gathering strings");
             }
-            Beta::Init(args) => {
+            Beta::Init(_) => {
                 // Init should create the appropriate strings folder and the base json file.
                 // it should have an optional parameter for doing the gather step too.
 
