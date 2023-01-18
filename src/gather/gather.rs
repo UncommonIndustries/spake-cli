0b625744bce4d7b45eb23106f44d46e20e20a896
Kiran Singh <ssnarik@gmail.com>
2023-01-17 17:13:54 -0800

    added base 64 encoding

--- src/gather/gather.rs
+++ src/gather/gather.rs
@@ -1,6 +1,8 @@
 // gather.rs
+use base64::{engine::general_purpose, Engine};
 use reqwest::Client;
-use std::error;
+use serde::{Deserialize, Serialize};
+use std::{error, fs};
 
 use crate::translate::translate;
 
@@ -19,10 +21,13 @@ pub async fn identify_strings_in_file(
             return Err(err.into());
         }
     };
-
+    let request_body = match build_gather_request_object(file_path) {
+        Ok(body) => body,
+        Err(err) => return Err(err.into()),
+    };
     let request = client
         .post(fqdn)
-        .json("hello world")
+        .json(&request_body)
         .header("X-API-KEY", api_key);
     let response = match request.send().await {
         Ok(res) => res,
@@ -32,3 +37,26 @@ pub async fn identify_strings_in_file(
 }
 
 // todo create a stringsRequestStructure.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct GatherRequestObject {
+    file_text: String,
+    file_path: String,
+}
+
+fn get_base64_encoded_string(data: String) -> String {
+    let encoded_data = general_purpose::STANDARD.encode(data);
+    return encoded_data;
+}
+
+fn build_gather_request_object(path: String) -> Result<GatherRequestObject, Box<dyn error::Error>> {
+    let contents = match fs::read_to_string(path.clone()) {
+        Ok(file_data) => file_data,
+        Err(err) => return Err(Box::new(err)),
+    };
+
+    let object = GatherRequestObject {
+        file_text: "todo!()".to_string(),
+        file_path: get_base64_encoded_string(contents),
+    };
+    Ok(object)
+}
